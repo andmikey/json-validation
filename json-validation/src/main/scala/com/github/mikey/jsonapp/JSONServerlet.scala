97ff24ee20c186ee6a9d8df7aca4374e4dc96bb3
Mikey <31766607+andmikey@users.noreply.github.com>
2018-05-03 13:32:44 +0100

    Add routing code in main serverlet

--- json-validation/src/main/scala/com/github/mikey/jsonapp/JSONServerlet.scala
+++ json-validation/src/main/scala/com/github/mikey/jsonapp/JSONServerlet.scala
@@ -4,8 +4,19 @@ import org.scalatra._
 
 class JSONServerlet extends ScalatraServlet {
 
-  get("/") {
-    views.html.hello()
+  post("/schema/:schemaid") {
+    val schemaid = params("schemaid");
+    <p> POST request for {schemaid} </p>
+  }
+
+  get("/schema/:schemaid") {
+    val schemaid = params("schemaid");
+    <p> GET request for {schemaid} </p>
+  }
+
+  post("/validate/:schemaid") {
+    val schemaid = params("schemaid");
+    <p> POST request for {schemaid} </p>
   }
 
 }
