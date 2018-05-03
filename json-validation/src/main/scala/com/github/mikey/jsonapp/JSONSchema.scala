f4cfc08a7b529b7d54aaf4b24326ecc4c73a0664
Mikey <31766607+andmikey@users.noreply.github.com>
2018-05-03 14:17:47 +0100

    Expand JSONSchema code
    Changes:
    - Add MongoDB support
    - Finer-grained JSON responses (specifiying schemaid)
    - Implement .add method

--- json-validation/src/main/scala/com/github/mikey/jsonapp/JSONSchema.scala
+++ json-validation/src/main/scala/com/github/mikey/jsonapp/JSONSchema.scala
@@ -2,20 +2,46 @@ package com.github.mikey.jsonapp
 
 import play.api.libs.json._
 
-class JSONSchema {
+// MongoDB imports
+import com.mongodb.casbah.Imports._
+
+class JSONSchema(mongoColl: MongoCollection) {
   // Class to hold instances of JSON schemas
 
   def add(schemaid: String, schema: String): JsValue = {
     // Add a schema to the database if not already in database
-    val successful_upload = (Json.obj("action" -> "uploadSchema", "id" -> "config-schema",
-      "status" -> "success"));
-    val invalid_upload = (Json.obj("action" -> "uploadSchema", "id" -> "config-schema",
-      "status" -> "error", "message" -> "Invalid JSON"));
-    val name_clash = (Json.obj("action" -> "uploadSchema", "id" -> "config-schema",
-      "status" -> "error", "message" -> "Schema with this name already exists"));
 
-    // Placeholder
-    return invalid_upload;
+    // Responses
+    val successful_upload = (Json.obj("action" -> "uploadSchema", "id" -> schemaid,
+      "status" -> "success"));
+    val invalid_upload = (Json.obj("action" -> "uploadSchema", "id" -> schemaid,
+      "status" -> "error", "message" -> "Invalid JSON"));
+    val db_issue = (Json.obj("action" -> "uploadSchema", "id" -> schemaid,
+      "status" -> "error", "message" -> "Cannot save to database"));
+
+    // Convert schema to JSON
+    val json_schema: JsValue =
+      try {
+        Json.parse(schema);
+      }
+      catch {
+        // Cannot parse - invalid JSON, return invalid response
+        case e: Exception => return invalid_upload;
+          null
+      }
+
+    // Save key:value to database
+    try {
+      val newObj = MongoDBObject(schema -> json_schema);
+      mongoColl += newObj;
+    }
+    catch {
+      // Something went wrong with saving to database
+      // TODO more fine-grained return code depending on db error
+      case e: Exception => return db_issue;
+    }
+
+    return successful_upload;
   }
 
   def get(schemaid: String): JsValue = {
@@ -25,9 +51,11 @@ class JSONSchema {
 
   def validate(schemaid:String, json: JsValue) : JsValue = {
     // Validate a JSON document against the named schema
-    val successful_validation = (Json.obj("action" -> "validateDocument", "id" -> "config-schema",
+
+    // Responses
+    val successful_validation = (Json.obj("action" -> "validateDocument", "id" -> schemaid,
       "status" -> "success"));
-    val invalid_validation = (Json.obj("action" -> "validateDocument", "id" -> "config-schema",
+    val invalid_validation = (Json.obj("action" -> "validateDocument", "id" -> schemaid,
       "status" -> "error", "message" -> "Could not validate JSON document against given schema"));
 
     // Placeholder
