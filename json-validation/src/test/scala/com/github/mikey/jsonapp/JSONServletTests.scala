b04cc7cb7c70eaa76371c6d586e00f671ac8f91d
Mikey <31766607+andmikey@users.noreply.github.com>
2018-05-05 11:58:06 +0100

    Add test files

--- json-validation/src/test/scala/com/github/mikey/jsonapp/JSONServletTests.scala
+++ json-validation/src/test/scala/com/github/mikey/jsonapp/JSONServletTests.scala
@@ -1,15 +1,31 @@
 package com.github.mikey.jsonapp
 
 import org.scalatra.test.scalatest._
+import org.scalatest.BeforeAndAfterAll
+import scala.io.Source
 
-class JSONServletTests extends ScalatraFunSuite {
+class JSONServletTests extends ScalatraFunSuite with BeforeAndAfterAll {
 
   addServlet(classOf[JSONServlet], "/*")
 
-  test("GET / on JSONServlet should return status 200") {
-    get("/") {
-      status should equal (200)
-    }
+  // Set up database with test data
+  override def beforeAll() {
+
   }
 
+  // Remove test data
+  override def afterAll() {
+
+  }
+
+  // test("GET / on JSONServlet should return status 200") {
+  //   get("/") {
+  //     status should equal (200)
+  //   }
+
+  val dir = System.getProperty("user.dir")
+  val json_schema = Source.fromFile(dir + "/files/config-schema.json").getLines.mkString
+  val valid_json_document = Source.fromFile(dir + "/files/config.json").getLines.mkString
+  val valid_with_null_json_document = Source.fromFile(dir + "/files/config-no-null.json").getLines.mkString
+  val invalid_json_docuemnt = Source.fromFile(dir + "/files/invalid.json").getLines.mkString
 }
