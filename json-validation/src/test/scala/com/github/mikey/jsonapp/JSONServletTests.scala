c6da29a0afe45cfd661a4e2eda3c632f89a7c1b3
Mikey <31766607+andmikey@users.noreply.github.com>
2018-05-05 12:03:54 +0100

    Set up database with test data when running tests

--- json-validation/src/test/scala/com/github/mikey/jsonapp/JSONServletTests.scala
+++ json-validation/src/test/scala/com/github/mikey/jsonapp/JSONServletTests.scala
@@ -3,6 +3,7 @@ package com.github.mikey.jsonapp
 import org.scalatra.test.scalatest._
 import org.scalatest.BeforeAndAfterAll
 import scala.io.Source
+import java.sql.{Connection, DriverManager, ResultSet}
 
 class JSONServletTests extends ScalatraFunSuite with BeforeAndAfterAll {
 
@@ -10,12 +11,28 @@ class JSONServletTests extends ScalatraFunSuite with BeforeAndAfterAll {
 
   // Set up database with test data
   override def beforeAll() {
+    val con_str = "jdbc:postgresql://localhost:5432/jsonapp?user=postgres"
+    val conn = DriverManager.getConnection(con_str)
 
+    try {
+      val stm = conn.createStatement()
+      val c = stm.executeUpdate(s"INSERT INTO jsonschemas VALUES ('config-schema', '$config_schema')");
+    } finally {
+      conn.close()
+    }
   }
 
   // Remove test data
   override def afterAll() {
+    val con_str = "jdbc:postgresql://localhost:5432/jsonapp?user=postgres"
+    val conn = DriverManager.getConnection(con_str)
 
+    try {
+      val stm = conn.createStatement()
+      val c = stm.executeUpdate(s"DELETE FROM jsonschemas where schemaid = 'config-schema'");
+    } finally {
+      conn.close()
+    }
   }
 
   // test("GET / on JSONServlet should return status 200") {
@@ -24,7 +41,7 @@ class JSONServletTests extends ScalatraFunSuite with BeforeAndAfterAll {
   //   }
 
   val dir = System.getProperty("user.dir")
-  val json_schema = Source.fromFile(dir + "/files/config-schema.json").getLines.mkString
+  val config_schema = Source.fromFile(dir + "/files/config-schema.json").getLines.mkString
   val valid_json_document = Source.fromFile(dir + "/files/config.json").getLines.mkString
   val valid_with_null_json_document = Source.fromFile(dir + "/files/config-no-null.json").getLines.mkString
   val invalid_json_docuemnt = Source.fromFile(dir + "/files/invalid.json").getLines.mkString
